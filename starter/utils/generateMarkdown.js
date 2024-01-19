a1dde5cf1a39c0ed55100ef141202ff3c1998293
Chantal Cassini-Jones <ccassinijones@gmail.com>
2024-01-19 22:40:26 +0000

    Refactor: \n add so that user data is underneath headings in READme file

--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,37 +1,22 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# <Your-Project-Title> ${data.title}
-  ## Description ${data.description}
+  return `# <Your-Project-Title> \n ${data.title}
+  ## Description \n ${data.description}
   ## Table of Contents: 
    - [Installation](#installation)
    - [Usage](#usage)
    - [License](#license)
-  ## Installation ${data.installation}
-  ## Usage ${data.usage}
-  ## Licence ${data.licence}
-  ## Contributing ${data.contribution}
-  ## Tests ${data.tests}
-  ## Questions ${data.github} ${data.email}
+  ## Installation \n ${data.installation}
+  ## Usage \n ${data.usage}
+  ## Licence \n ${data.licence}
+  ## Contributing \n ${data.contribution}
+  ## Tests \n ${data.tests}
+  ## Questions \n ${data.github} ${data.email}
 
 
 `;
 }
-//const util = require('util');
-//const writeFileAsync = util.promisify(fs.writeFile);
-//module.exports = generateMarkdown;
-// .then((data) => generateMarkdown(data));
 
-// questions()
-//   .then((data) => writeFileAsync('README.md', generateMarkdown(data)))
-//   .then(() => console.log('Successfully wrote to README.md'))
-//   .catch((err) => console.error(err));
+module.exports = generateMarkdown;
+
 //make badges function and links for licence
-
-
-
-
-
-
-
-
-
