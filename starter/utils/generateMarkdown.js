47abd9da26d9a03cf00a9aeb4c5d313ce884995f
Chantal Cassini-Jones <ccassinijones@gmail.com>
2024-01-19 22:13:22 +0000

    Feat: add markdown syntax to generateMarkdown function

--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,8 +1,37 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# <Your-Project-Title> ${data.title}
+  ## Description ${data.description}
+  ## Table of Contents: 
+   - [Installation](#installation)
+   - [Usage](#usage)
+   - [License](#license)
+  ## Installation ${data.installation}
+  ## Usage ${data.usage}
+  ## Licence ${data.licence}
+  ## Contributing ${data.contribution}
+  ## Tests ${data.tests}
+  ## Questions ${data.github} ${data.email}
+
 
 `;
 }
+//const util = require('util');
+//const writeFileAsync = util.promisify(fs.writeFile);
+//module.exports = generateMarkdown;
+// .then((data) => generateMarkdown(data));
 
-module.exports = generateMarkdown;
+// questions()
+//   .then((data) => writeFileAsync('README.md', generateMarkdown(data)))
+//   .then(() => console.log('Successfully wrote to README.md'))
+//   .catch((err) => console.error(err));
+//make badges function and links for licence
+
+
+
+
+
+
+
+
+
